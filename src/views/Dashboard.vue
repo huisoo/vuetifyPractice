4de6bb0015a6d1c09ce086283a61fdbb7a735e96
huisoo.lee <huisoo.lee@huisoolees-MacBook-Pro.local>
2021-12-08 14:31:22 +0900

    first-commit vuetify

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-card>
+        대시보드
+      </v-card>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
---
+++ src/views/GridSystem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-card class="pa-3">
+        <v-row>
+          <v-col>
+            <v-card
+              class="pa-3"
+              outlined
+            >
+              Col
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card
+              class="pa-3"
+              outlined
+            >
+              Col
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card
+              class="pa-3"
+              outlined
+            >
+              Col
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card
+              class="pa-3"
+              outlined
+            >
+              Col
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card
+              class="pa-3"
+              outlined
+            >
+              Col
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+
+</style>
